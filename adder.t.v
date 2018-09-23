7895b51c302ea2d6b8da1002f77958b7169f059c
QingmuDeng <qingmu.deng@students.olin.edu>
2018-09-23 11:16:06 -0400

    first run test bench

---
+++ adder.t.v
@@ -0,0 +1,36 @@
+`timescale 1 ns / 1 ps
+`include "adder.v"
+
+module testFullAdder();
+    reg[3:0] a, b;
+    wire[3:0] sum;
+    wire carryout, overflow;
+
+    // behavioralFullAdder adder (sum, carryout, a, b, carryin);
+    FullAdder4bit adder (sum, carryout, overflow, a, b);
+
+    initial begin
+        a = 0; b = 0; #100
+        $dumpfile("adder.vcd");
+        $dumpvars(0, adder);
+
+        $display("  a      b    | Cout  Sum  Overflow | Expected Output");     // Header for the truth table for nAandnB
+        a = 4'b0000; b = 4'b0000; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   0", a, b, carryout, sum, overflow);
+        a = 4'b0001; b = 4'b0001; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   2", a, b, carryout, sum, overflow);
+        a = 4'b0111; b = 4'b1111; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   6", a, b, carryout, sum, overflow);
+        a = 4'b1000; b = 4'b0111; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   -1", a, b, carryout, sum, overflow);
+        a = 4'b1110; b = 4'b1101; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   -5", a, b, carryout, sum, overflow);
+        a = 4'b0001; b = 4'b0011; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   4", a, b, carryout, sum, overflow);
+        $display("\nTest Overflow:");
+        a = 4'b0011; b = 4'b0110; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   9/Overflow", a, b, carryout, sum, overflow);
+        a = 4'b1100; b = 4'b1011; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   -9/Overflow", a, b, carryout, sum, overflow);
+    end
+endmodule
--- adder.v
+++ adder.v
@@ -1,29 +1,5 @@
 // Adder circuit
-`define NOT not #50
-`define AND and #50
-`define OR or #50
-`define NAND nand #50
-`define NOR nor #50
-`define XOR xor #50
-
-module structuralFullAdder
-(
-    output sum, 
-    output carryout,
-    input a, 
-    input b, 
-    input carryin
-);
-    // Your adder code here
-    wire G, P, PandCin;
-
-    `AND generator(G, a, b);
-    `XOR propagate(P, a,b);
-
-    `AND carry(PandCin, P, carryin);
-    `OR Cout(carryout, PandCin, G);
-    `XOR summation(sum, P, carryin);
-endmodule
+`include "adder_1bit.v"
 
 module FullAdder4bit
 (
@@ -35,10 +11,10 @@ module FullAdder4bit
 );
     // Your Code Here
     wire[2:0] Cout;
-    structuralFullAdder add0(sum[0], Cout[0], a[0], b[0], 0b'0);
+    structuralFullAdder add0(sum[0], Cout[0], a[0], b[0], 1'b0);
     structuralFullAdder add1(sum[1], Cout[1], a[1], b[1], Cout[0]);
-    structuralFullAdder add1(sum[2], Cout[2], a[2], b[2], Cout[1]);
-    structuralFullAdder add1(sum[3], carryout, a[3], b[3], Cout[2]);
+    structuralFullAdder add2(sum[2], Cout[2], a[2], b[2], Cout[1]);
+    structuralFullAdder add3(sum[3], carryout, a[3], b[3], Cout[2]);
 
     `XOR overflow_check(overflow, carryout, Cout[2]);
 endmodule
