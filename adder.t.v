2fc6afceb471ccfdf909191f879097100bf856fa
QingmuDeng <qingmu.deng@students.olin.edu>
2018-09-24 19:39:54 -0400

    updated test bench, two more cases needed

--- adder.t.v
+++ adder.t.v
@@ -17,20 +17,32 @@ module testFullAdder();
         $display("  a      b    | Cout  Sum  Overflow | Expected Output");     // Header for the truth table for nAandnB
         a = 4'b0000; b = 4'b0000; #1000
         $display(" %b   %b  |  %b    %b    %b     |   0", a, b, carryout, sum, overflow);
+        a = 4'b1111; b = 4'b1111; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   -2", a, b, carryout, sum, overflow);
         a = 4'b0001; b = 4'b0001; #1000
         $display(" %b   %b  |  %b    %b    %b     |   2", a, b, carryout, sum, overflow);
+        a = 4'b0001; b = 4'b0011; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   4", a, b, carryout, sum, overflow);
         a = 4'b0111; b = 4'b1111; #1000
         $display(" %b   %b  |  %b    %b    %b     |   6", a, b, carryout, sum, overflow);
         a = 4'b1000; b = 4'b0111; #1000
         $display(" %b   %b  |  %b    %b    %b     |   -1", a, b, carryout, sum, overflow);
         a = 4'b1110; b = 4'b1101; #1000
         $display(" %b   %b  |  %b    %b    %b     |   -5", a, b, carryout, sum, overflow);
-        a = 4'b0001; b = 4'b0011; #1000
-        $display(" %b   %b  |  %b    %b    %b     |   4", a, b, carryout, sum, overflow);
+        a = 4'b0101; b = 4'b1011; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   0", a, b, carryout, sum, overflow);
+        a = 4'b0110; b = 4'b0000; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   6", a, b, carryout, sum, overflow);
+        a = 4'b1011; b = 4'b0000; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   -5", a, b, carryout, sum, overflow);
         $display("\nTest Overflow:");
         a = 4'b0011; b = 4'b0110; #1000
         $display(" %b   %b  |  %b    %b    %b     |   9/Overflow", a, b, carryout, sum, overflow);
+        a = 4'b0111; b = 4'b0111; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   14/Overflow", a, b, carryout, sum, overflow);
         a = 4'b1100; b = 4'b1011; #1000
         $display(" %b   %b  |  %b    %b    %b     |   -9/Overflow", a, b, carryout, sum, overflow);
+        a = 4'b1000; b = 4'b1000; #1000
+        $display(" %b   %b  |  %b    %b    %b     |   -16/Overflow", a, b, carryout, sum, overflow);
     end
 endmodule
