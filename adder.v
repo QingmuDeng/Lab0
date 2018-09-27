ecabdb1e96fc0b53280d14d4b3e1d24385522d55
Qingmu Deng <31523537+QingmuDeng@users.noreply.github.com>
2018-09-27 11:39:09 -0400

    commenting

--- adder.v
+++ adder.v
@@ -11,10 +11,16 @@ module FullAdder4bit
 );
     // Your Code Here
     wire[2:0] Cout;
+  
+    // Four bit adder in ripple-carry style 
     structuralFullAdder add0(sum[0], Cout[0], a[0], b[0], 1'b0);
+    // In ripple carry adder, the carry out of the last 1-bit adder is
+    // the carry in of the current 1-bit adder
     structuralFullAdder add1(sum[1], Cout[1], a[1], b[1], Cout[0]);
     structuralFullAdder add2(sum[2], Cout[2], a[2], b[2], Cout[1]);
     structuralFullAdder add3(sum[3], carryout, a[3], b[3], Cout[2]);
-
+    
+    // An overflow occurs if the carry in of the last 1-bit adder in the
+    // ripple carry chain is different from its carry out.
     `XOR overflow_check(overflow, carryout, Cout[2]);
 endmodule
